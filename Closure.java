b5d3c467522fabb98861c65cc8980d7c15e063c7
juliefruity <60415297+juliefruity@users.noreply.github.com>
2020-07-30 21:55:55 -0700

    Create Closure.java

---
+++ Closure.java
@@ -0,0 +1,14 @@
+package codecakesendpack;   //import package.
+
+//Output and called from bye.msg() in Codecakes.java.
+public class Closure {
+  public void msg() {
+   System.out.println("*Order has been successfully sent!*\nPayments processed through store pick-up or upon delivery.");
+   System.out.println();
+   System.out.println("Thank you for your service at Codecakes!\nEnjoy!");
+   System.out.println();
+   System.out.print("(c)Pro-J.");
+   System.out.println();
+  }
+
+}
